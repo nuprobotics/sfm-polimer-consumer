051555e76019cc654a5f8306a8660f3533174ad4
Evgenii <jenyhlevnoy@gmail.com>
2024-11-14 20:15:47 +0200

    task 2 solved

--- cv2_solution.py
+++ cv2_solution.py
@@ -1,24 +1,42 @@
 import numpy as np
 import cv2
 import typing
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
-import yaml
+from typing import Tuple, Sequence
 
 
-# Task 2
-def get_matches(image1, image2) -> typing.Tuple[
-    typing.Sequence[cv2.KeyPoint], typing.Sequence[cv2.KeyPoint], typing.Sequence[cv2.DMatch]]:
+def get_matches(
+        image1: np.ndarray,
+        image2: np.ndarray,
+        k_ratio: float = 0.75
+) -> Tuple[Sequence[cv2.KeyPoint], Sequence[cv2.KeyPoint], Sequence[cv2.DMatch]]:
     sift = cv2.SIFT_create()
+
     img1_gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     img2_gray = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+
     kp1, descriptors1 = sift.detectAndCompute(img1_gray, None)
     kp2, descriptors2 = sift.detectAndCompute(img2_gray, None)
 
     bf = cv2.BFMatcher()
-    matches_1_to_2: typing.Sequence[typing.Sequence[cv2.DMatch]] = bf.knnMatch(descriptors1, descriptors2, k=2)
 
-    # YOUR CODE HERE
+    matches_1_to_2 = bf.knnMatch(descriptors1, descriptors2, k=2)
+    matches_2_to_1 = bf.knnMatch(descriptors2, descriptors1, k=2)
+
+    good_matches_1_to_2 = [
+        m[0] for m in matches_1_to_2 if len(m) == 2 and m[0].distance < k_ratio * m[1].distance
+    ]
+    good_matches_2_to_1 = [
+        m[0] for m in matches_2_to_1 if len(m) == 2 and m[0].distance < k_ratio * m[1].distance
+    ]
+
+    final_matches = []
+    for m1 in good_matches_1_to_2:
+        for m2 in good_matches_2_to_1:
+            if m1.queryIdx == m2.trainIdx and m1.trainIdx == m2.queryIdx:
+                final_matches.append(m1)
+                break
+
+    return kp1, kp2, final_matches
 
 
 def get_second_camera_position(kp1, kp2, matches, camera_matrix):
@@ -69,7 +87,6 @@ def visualisation(
         camera_position3: np.ndarray,
         camera_rotation3: np.ndarray,
 ):
-
     def plot_camera(ax, position, direction, label):
         color_scatter = 'blue' if label != 'Camera 3' else 'green'
         # print(position)
@@ -79,7 +96,6 @@ def visualisation(
         ax.quiver(position[0][0], position[1][0], position[2][0], direction[0], direction[1], direction[2],
                   length=1, color=color_quiver, arrow_length_ratio=0.2)
         ax.text(position[0][0], position[1][0], position[2][0], label, color='black')
-
 
     camera_positions = [camera_position1, camera_position2, camera_position3]
     camera_directions = [camera_rotation1[:, 2], camera_rotation2[:, 2], camera_rotation3[:, 2]]
@@ -109,7 +125,6 @@ def visualisation(
     ax_azim_slider = plt.axes([0.1, 0.05, 0.65, 0.03])
     azim_slider = Slider(ax_azim_slider, 'Azim', 0, 360, valinit=initial_azim)
 
-
     def update(val):
         elev = elev_slider.val
         azim = azim_slider.val
@@ -134,7 +149,7 @@ def main():
     R2, t2, E = get_second_camera_position(key_points1, key_points2, matches_1_to_2, camera_matrix)
     triangulated_points = triangulation(
         camera_matrix,
-        np.array([0, 0, 0]).reshape((3,1)),
+        np.array([0, 0, 0]).reshape((3, 1)),
         np.eye(3),
         t2,
         R2,
@@ -144,7 +159,7 @@ def main():
     )
 
     R3, t3 = resection(image1, image3, camera_matrix, matches_1_to_2, triangulated_points)
-    camera_position1, camera_rotation1 = convert_to_world_frame(np.array([0, 0, 0]).reshape((3,1)), np.eye(3))
+    camera_position1, camera_rotation1 = convert_to_world_frame(np.array([0, 0, 0]).reshape((3, 1)), np.eye(3))
     camera_position2, camera_rotation2 = convert_to_world_frame(t2, R2)
     camera_position3, camera_rotation3 = convert_to_world_frame(t3, R3)
     visualisation(
@@ -155,6 +170,7 @@ def main():
         camera_position3,
         camera_rotation3
     )
+
 
 if __name__ == "__main__":
     main()
